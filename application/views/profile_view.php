ce1bf8a46e725804d56661b35356049a9fea66e1
Jessica Wilson <jessicalostinspace@gmail.com>
2016-02-25 10:27:43 -0800

    changed profile

--- application/views/profile_view.php
+++ application/views/profile_view.php
@@ -3,227 +3,10 @@
 	if(!$this->session->userdata( 'fb_access_token' )){
 		redirect("/");
 	}
+
 ?>
 
 
-<style type="text/css">
-	.profile_images{
-		height:350px;
-	}
-  		/*The z-index property specifies the stack order of an element.
-		An element with greater stack order is always in front of an element with a lower stack order.*/
-	.profile_background{
-		z-index: -1;
-	}
-	.profile_header{
-		opacity: 0.4;
-  		background-color: #030503;
-  		width: 1000px;
-  		height: 75px;
-		z-index: 1;
-		position: relative;
-		top: -75px;
-		right: 70px;
-	}
-	.profile_picture{
-		z-index: 2;
-		position: relative;
-		border-radius: 10px;
-		bottom: 130px;
-		left: 48px;
-	}
-	.username_display{
-		position: relative;
-		bottom: 93px;
-		left: 69px;
-		display:inline;
-	}
-	.headerinfo{
-		color: white;
-		z-index: 3;
-		position: relative;
-		left: -245px;
-		top: 15px;
-		margin: 0;
-		text-align: center;
-	}
-	.headerinfo2{
-	    color: white;
-	    z-index: 4;
-	    position: relative;
-	    left: -101px;
-	    top: -31px;
-	    margin: 0;
-	    text-align: center;
-	}
-	#upcoming_events{
-		position: relative;
-		left: 215px;
-		bottom: -30px;
-		width: 495px;
-	}
-	.panel{
-		border-color: #043505;
-	}
-	.panel-heading{
-		border-bottom-style: solid;
-		border-color: #043505;
-	}
-	.message{
-	    background-color: #043505;
-	    height: 39px;
-	    width: 120px;
-	    position: relative;
-	    bottom: 123px;
-	    left: -147px;
-	}
-	#description{
-		width: 196px;
-	    position: relative;
-	    left: 2px;
-	    bottom: 201px;
-	}
-	.edit{
-		font-size: 14px;
-	}
-    .modal-header, .modal_header, .close {
-      background-color: #043505;
-      color:white !important;
-      text-align: center;
-      font-size: 30px;
-  	}
-    .modal-footer {
-      background-color: #f9f9f9;
-    }
-    .location_btn{
-    	background-color: #043505;
-    }
-    p.location{
-    	font-style: oblique;
-    }
-    #local_events{
-	    width: 263px;
-	    position: relative;
-	    left: -150px;
-	    bottom: 519px;
-	    float: right;
-    }
-    .borders{
-    	border: 1px solid #043505;
-    	background-color: white;
-    	border-radius: 5px;
-    }
-    .aboutme{
-	    position: relative;
-	    left: 17px;
-	    bottom: 68px;
-    }
-    .future_events{
-		position: relative;
-	    right: 65px;
-	    bottom: 120px;
-    }
-    .events_near_you{
-	    position: relative;
-	    right: 146px;
-	    bottom: 120px;
-    }
-    .events_near_you h2{
-		border-bottom-style: solid;
-		border-color: black;
-		border-width: 1px;
-		padding-bottom: 8px;
-    }
-    .future_events h2{
-		border-bottom-style: solid;
-		border-color: black;
-		border-width: 1px;
-		padding-bottom: 8px;
-    }
-    .past_events{
-    	position: relative;
-	    right: 63px;
-	    bottom: 100px;
-    }
-    .past_events h2{
-		border-bottom-style: solid;
-		border-color: black;
-		border-width: 1px;
-		padding-bottom: 8px;
-    }
-    .forest{
-    	color: #015604;
-    }
-    .ajaxmsg{
-		position: relative;
-	    right: 36px;
-	    bottom: 114px;
-    }
-</style>
-
-
-<!-- <div class="container">
-
-	<div class="profile">
-		<div class="profile_images">
-			<img class="profile_background .img-responsive" src="/assets/images/treesprofile.jpeg" alt="Trees Default" style="width:1000px;height:300px;">
-			<div class="container profile_header">				
-
-					<h6 class="headerinfo">Completed Hikes</h6>
-					<h2 class="headerinfo">5</h2>
-
-			</div>
-			<img class="profile_picture .img-rounded" src="<?= $user['picture_url']?>" alt="Profile picture" style="width:120px; height:120px;">
-			<h1 style="display:inline"><?= $user['first_name']." ".$user['last_name']?></h1>
-		</div>	 
-	</div>
-
-	<div id="upcoming_events" class="panel panel-success">
-		<div class="panel-heading">
-			Upcoming Events
-		</div>
-		<div class="panel-body">
-
-			<h4><a href="#">No, motherfucker</a></h4>
-			<p>Look, just because I don't be givin' no man a foot massage don't make it right for Marsellus to throw Antwone into a glass motherfuckin' house, fuckin' up the way the nigger talks. Motherfucker do that shit to me, he better paralyze my ass, 'cause I'll kill the motherfucker, know what I'm sayin'? </p>
-
-
-		</div>
-	</div>
-
- 
- <a class="message btn btn-success btn-xs" href="/all"><h5>Message</h5></a>
-
-	 <div id="description" class="panel panel-default">
-	  <div class="panel-body">
-	    <h3>About<a class="edit" style="margin-left: 50px;" href="#">Edit</a></h3>
-	    <p class="location"><?= $user['location'] ?></p>
-	    <p></p>
-	    <p><?= $user['description'] ?></p>
-	  </div>
-	</div>
-
-	<div id="local_events" class="panel panel-success">
-		<div class="panel-heading">
-			Events near you
-		</div>
-		<div class="panel-body">
-
-			<h5><a href="#">Mt.Rainier this Tuesday!!!</a></h5>
-			<p><i>Tues, May 25</i></p>
-			<p>Location: Paradise-Rainier (295 mi. from you)</p>
-			<p><b>Length:</b> 8 mi. <b>Elev. Gain:</b> 2500 ft.</p>
-			<p></p>
-			<h5><a href="#">Mt.Rainier next Tuesday!!!</a></h5>
-			<p><i>Tues, May 31</i></p>
-			<p>Location: Paradise-Rainier (295 mi. from you)</p>
-			<p><b>Length:</b> 8 mi. <b>Elev. Gain:</b> 2500 ft.</p>
-
-
-		</div>
-	</div>
-</div>
- -->
 <div class="container">
   <!-- Modal -->
   <div class="modal fade" id="myModal" role="dialog">
@@ -295,7 +78,7 @@ $(document).ready(function(){
 	<div class="container">
 		<div class="images row">
 	    	<div class="col-md-12">
-	    		<img class="profile_background .img-responsive" src="/assets/images/treesprofile.jpeg" alt="Trees Default" style="width:1000px;height:300px;">
+	    		<img class="profile_background .img-responsive" src="/assets/images/treesprofile.jpeg" alt="Trees Default">
 				<div class="container profile_header">				
 
 					<h6 class='headerinfo'>Completed Hikes</h6>
@@ -303,24 +86,24 @@ $(document).ready(function(){
 
 					<div style="display: inline;">
 						<h6 class='headerinfo2'>New Messages</h6>
-						<h2 class='headerinfo2'><a id="newMsg" style="color:#ff0000;" href="/messages/showPersonal">7</a></h2>
+						<h2 class='headerinfo2'><a id="newMsg" href="/messages/showPersonal">7</a></h2>
 					</div>
 				</div>
-				<img class="profile_picture .img-rounded" src="<?= $user['picture_url']?>" alt="Profile picture" style="width:120px; height:120px;">
+				<img class="profile_picture .img-rounded" src="<?= $user['picture_url']?>" alt="Profile picture">
 				<h1 class="username_display"><?= $user['first_name']." ".$user['last_name']?></h1>
 	    	</div>
 		</div>
 		<div class="row">
-		 	<div class="aboutme borders col-md-2">
+		 	<div class="aboutme borders col-sm-2">
 		 		<h3 class="forest">
-			 		About<a class="edit" style="margin-left: 50px;" href="#">Edit</a>
+			 		About<a class="edit" href="#">Edit</a>
 		 		</h3>
 			    <p class="location"><?= $user['location'] ?></p>
 			    <p></p>
 			    <p><?= $user['description'] ?></p>
 		 	</div>
 <div id="replaced">		    
-		    <div class="col-md-1">
+		    <div class="col-sm-1">
 		    	<a class="message btn btn-success btn-xs" href="#"><h5>Message</h5></a>
 		    </div>
 			<div class="future_events borders col-md-5">
@@ -335,10 +118,10 @@ $(document).ready(function(){
 				<h4><a href="#">No, motherfucker</a></h4>
 				<p>Look, just because I don't be givin' no man a foot massage don't make it right for Marsellus to throw Antwone into a glass motherfuckin' house, fuckin' up the way the nigger talks. Motherfucker do that shit to me, he better paralyze my ass, 'cause I'll kill the motherfucker, know what I'm sayin'? </p>
 		    </div>
-		    <div class="col-md-1">
+		    <div class="col-sm-1">
 		    	
 		    </div>
-		    <div class="events_near_you borders col-md-3">
+		    <div class="events_near_you borders col-sm-3">
 		    	<h2 class="forest">Hikes near you</h2>
 
 				<h5><a href="#">Mt.Rainier this Tuesday!!!</a></h5>
@@ -353,8 +136,8 @@ $(document).ready(function(){
 
 		    </div>
 	 	<div class="row">
-		 	<div class="col-md-3"></div>
-	 		<div class="past_events borders col-md-5">
+		 	<div class="col-sm-3"></div>
+	 		<div class="past_events borders col-sm-5">
 				<h2 class="forest">Past Hikes</h2>
 
 				<h4><a href="#">Yes, motherfucker</a></h4>
@@ -363,7 +146,7 @@ $(document).ready(function(){
 		    </div>
 		</div>
 	 	<div class="row">
-	 		<div class="borders col-md-10">
+	 		<div class="borders col-sm-12">
 		 		<label class="control-label"><h2>Snap a cool pic on the trail? Upload it!</h2></label>
 				<input id="input-1" type="file" class="file">
 				<p></p>
